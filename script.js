57f6f8c9b14f4270fe440be6cb6027c7540077f5
Anasscodes <rockbell8@gmail.com>
2021-07-05 18:21:02 +0300

    Add lint fix

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function newbook() {
-  const Title = document.getElementById("Title")
-  const Author = document.getElementById("Author")
-  let newdiv = document.createElement("div")
-  newdiv.setAttribute("id", "book_infos")
-  const par1 = document.createElement("p");
-  const par2 = document.createElement("p");
-  const buutt = document.createElement("button");
-  buutt.textContent = "remove";
-  buutt.setAttribute("id", "rem")
+  const Title = document.getElementById('Title');
+  const Author = document.getElementById('Author');
+  let newdiv = document.createElement('div');
+  newdiv.setAttribute('id', 'book_infos');
+  const par1 = document.createElement('p');
+  const par2 = document.createElement('p');
+  const buutt = document.createElement('button');
+  buutt.textContent = 'remove';
+  buutt.setAttribute('id', 'rem');
   par1.appendChild(document.createTextNode(Title.value));
   par2.appendChild(document.createTextNode(Author.value));
-  document.getElementById("list_container").appendChild(newdiv).appendChild(par1)
-  document.getElementById("list_container").appendChild(newdiv).appendChild(par2)
-  document.getElementById("list_container").appendChild(newdiv).appendChild(buutt)
+  document.getElementById('list_container').appendChild(newdiv).appendChild(par1);
+  document.getElementById('list_container').appendChild(newdiv).appendChild(par2);
+  document.getElementById('list_container').appendChild(newdiv).appendChild(buutt);
 }
 
 function newbook_remove() {
-  const book_container = document.getElementById("book_infos");
-  book_container.parentElement.removeChild(book_container)
+  const book_container = document.getElementById('book_infos');
+  book_container.parentElement.removeChild(book_container);
 }
