39c33cc25a432df543a4ac58fbbf492614afa151
Anasscodes <rockbell8@gmail.com>
2021-07-05 17:52:50 +0300

    Add lint fix

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function newbook() {
     var title_name = document.getElementById("Title")
     var Author_name = document.getElementById("Author")
-    var newdiv=document.createElement("div")
+    var newdiv = document.createElement("div")
     newdiv.setAttribute("id", "book_infos")
     var par1 = document.createElement("p");
     var par2 = document.createElement("p");
-    var buutt= document.createElement("button");
-    buutt.textContent="remove";
-    buutt.setAttribute("id","rem")
+    var buutt = document.createElement("button");
+    buutt.textContent = "remove";
+    buutt.setAttribute("id", "rem")
     par1.appendChild(document.createTextNode(title_name.value));
     par2.appendChild(document.createTextNode(Author_name.value));
     document.getElementById("list_container").appendChild(newdiv).appendChild(par1)
@@ -15,12 +15,7 @@ function newbook() {
     document.getElementById("list_container").appendChild(newdiv).appendChild(buutt)
 }
 
-//$(document).on('click','#rem',function(){alert("ssafasf")})
-// document.getElementById("rem").addEventListener("click", remove_book());
-// document.querySelector("#list_container").addEventListener('click', remove_book())
-
-
-function newbook_remove () {
+function newbook_remove() {
     var book_container = document.getElementById("book_infos");
     book_container.parentElement.removeChild(book_container)
 }
