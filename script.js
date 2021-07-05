9e1dd615e66fb48fd38b4c254d04469e746f7047
Anasscodes <rockbell8@gmail.com>
2021-07-05 13:34:47 +0300

    Add event listener

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function newbook() {
     var title_name = document.getElementById(Title)
     var Author_name = document.getElementById(Author)
     var newdiv = document.createElement("div")
+
     newdiv.appendChild(title_name)
     newdiv.appendChild(Author_name) 
     console.log("test")  
