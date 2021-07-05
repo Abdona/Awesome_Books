89985899b574a2ca8613e01afa3683d8698aeab1
Anasscodes <rockbell8@gmail.com>
2021-07-05 18:06:04 +0300

    Add minor modifications

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function newbook() {
-    var title_name = document.getElementById("Title")
-    var Author_name = document.getElementById("Author")
-    var newdiv = document.createElement("div")
+    let Title = document.getElementById("Title")
+    let Author = document.getElementById("Author")
+    let newdiv = document.createElement("div")
     newdiv.setAttribute("id", "book_infos")
-    var par1 = document.createElement("p");
-    var par2 = document.createElement("p");
-    var buutt = document.createElement("button");
+    let par1 = document.createElement("p");
+    let par2 = document.createElement("p");
+    let buutt = document.createElement("button");
     buutt.textContent = "remove";
     buutt.setAttribute("id", "rem")
-    par1.appendChild(document.createTextNode(title_name.value));
-    par2.appendChild(document.createTextNode(Author_name.value));
+    par1.appendChild(document.createTextNode(Title.value));
+    par2.appendChild(document.createTextNode(Author.value));
     document.getElementById("list_container").appendChild(newdiv).appendChild(par1)
     document.getElementById("list_container").appendChild(newdiv).appendChild(par2)
     document.getElementById("list_container").appendChild(newdiv).appendChild(buutt)
 }
 
 function newbook_remove() {
-    var book_container = document.getElementById("book_infos");
+    let book_container = document.getElementById("book_infos");
     book_container.parentElement.removeChild(book_container)
 }
 
