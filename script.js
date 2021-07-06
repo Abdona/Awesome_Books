9aac0dbfedea81bc3d6a0b877fbfd1f9a5196019
Abdulrahman <boodynaser@rocketmail.com>
2021-07-06 11:35:26 +0200

    Fix Remove process,Use Array Objects to store the Books inside

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-let Books=[];
-function add_book(title,author) {
-  Books.push({title,author,id:Math.floor(Math.random())})
-  const par1 = document.createElement('p');
-  const par2 = document.createElement('p');
-  const remove_butt = document.createElement('button');
-  remove_butt = 'remove';
-  remove_butt.addEventListener('click',alert("hello"))
-  par1.appendChild(document.createTextNode(Title.value));
-  par2.appendChild(document.createTextNode(Author.value));
+const Books = [];
+function NewbookRemove(RButton) {
+  // const ID = GetClickedId();
+  const BookContainer = document.getElementById(RButton.id);
+  BookContainer.parentNode.removeChild(BookContainer);
 }
 function Newbook() {
   const Title = document.getElementById('Title');
   const Author = document.getElementById('Author');
+  const IdBook = Math.floor(Math.random() * 1000);
+  Books.push({ title: Title.value, author: Author.value, id: IdBook });
+  // const BookLength = document.createElement('p');
+  // BookLength.appendChild(document.createTextNode(Books.length));
   const newdiv = document.createElement('div');
-  // newdiv.setAttribute('id', 'book_infos');
-  add_book(Title,Author);
-
-  // document.getElementById('list_container').appendChild(newdiv).appendChild(par1);
-  // document.getElementById('list_container').appendChild(newdiv).appendChild(par2);
-  // document.getElementById('list_container').appendChild(newdiv).appendChild(buutt);
-}
-  console.log("Ssss")
-  const sumbit_butt=document.getElementById("Submit");
-  sumbit_butt.onclick= function() { console.log("dddd")}
-  function Newbook_Remove() {
-  const Book_Container = document.getElementById('book_infos');
-  Book_Container.parentElement.removeChild(Book_Container);
+  newdiv.setAttribute('id', IdBook);
+  const par1 = document.createElement('p');
+  const par2 = document.createElement('p');
+  const RemoveButt = document.createElement('button');
+  RemoveButt.addEventListener('click', () => { NewbookRemove(RemoveButt); });
+  RemoveButt.textContent = 'remove';
+  RemoveButt.setAttribute('id', IdBook);
+  par1.appendChild(document.createTextNode(Title.value));
+  par2.appendChild(document.createTextNode(Author.value));
+  // document.getElementById('list_container').appendChild(newdiv).appendChild(BookLength);
+  document.getElementById('list_container').appendChild(newdiv).appendChild(par1);
+  document.getElementById('list_container').appendChild(newdiv).appendChild(par2);
+  document.getElementById('list_container').appendChild(newdiv).appendChild(RemoveButt);
 }
