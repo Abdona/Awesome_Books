bb60bf5eb63c34c9c7d9effd8b519b7e04b074a3
Abdulrahman <boodynaser@rocketmail.com>
2021-07-07 16:39:16 +0200

    Add to  localstorage

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-class Storage{
+class LocalStorage{
   constructor(name){
     this.name=name;
-    localStorage.setItem(this.name)
+    // BooksCollection= JSON.parse(localStorage.getItem(this.name)) || [];
+    // alert("sss")
+    localStorage.setItem(this.name,'');
+    // alert("sss")
   }
   AddToStorage(Data)
   {
-    alert('ss')
+    // Data.push(JSON.parse(BookCollection));
     localStorage.setItem(this.name,JSON.stringify(Data));
   }
+  RemoveBook(Books,RButton) {
+    const BookContainer = document.getElementById(RButton.id);
+    BookContainer.parentNode.removeChild(BookContainer);
+    /* eslint-disable */ 
+    const BooksNew = Books.filter(function (item) {
+      return item.id != RButton.id;
+    });
+    /* eslint-enable */
+    Books = BooksNew;
+    localStorage.clear();
+    const BookList = JSON.stringify(BooksNew);
+    // eslint-disable-next-line no-unused-vars
+    localStorage.setItem('library', BookList);
+  }
+
+
 }
-class Books {
+class Book {
   constructor(title,author,id){
     this.title = title;
     this.author = author;
     this.id = id;
     let Book={title: this.title, author: this.author,id: this.id}
-    let BookListArray = [];
-    BookListArray.push(Book);
+    let BookListArray = Book;
   }
 
   AddBook() {
        let TitleP = document.createElement('p');
        let AuthorP = document.createElement('p')
        let RButton = document.createElement('button')
+      //  alert('hhhhh')
+      //  RemoveButt.addEventListener('click', () => { NewbookRemove(RemoveButt); });
        RButton.innerHTML = "Remove"
        RButton.setAttribute('id', this.id) 
        let BookContainer = document.createElement('div')
@@ -32,25 +52,21 @@ class Books {
        document.getElementById('list_container').appendChild(BookContainer).appendChild(TitleP);
        document.getElementById('list_container').appendChild(BookContainer).appendChild(AuthorP);
        document.getElementById('list_container').appendChild(BookContainer).appendChild(RButton);
-       this.SaveData();
-
-  }
-  SaveData() {
-    BookCollection = new Storage ('library');
-    BookCollection.AddToStorage(BookListArray);
-  }
-
+      }
 }
 
 function AddNewbook() {
   const title = document.getElementById('Title');
   const author = document.getElementById('Author');
   const IdBook = Math.floor(Math.random() * 1000);
-  let NewBook = new Books(title.value, author.value, IdBook);
+  let NewBook = new Book(title.value, author.value, IdBook);
+  // alert('ddd')
   NewBook.AddBook();
-  
+  let BookArray = new Array();
+  BookArray.push(NewBook);
+  const storge = new LocalStorage('library');
+  storge.AddToStorage(BookArray);
 }
-
 
 // let Books = [];
 // function NewbookRemove(RButton) {
